0ad8bdff0cf6fbf991b13784878410c2aa0bfab4
ericstober <erictstober@gmail.com>
2026-05-05 22:48:52 -0500

    add token metadata

--- main.py
+++ main.py
@@ -15,12 +15,23 @@ def main():
     # Create new instance of a Gemini client
     client = genai.Client(api_key=api_key)
 
-    res = client.models.generate_content(
+    # Call client with model and prompt. Captured as response
+    response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
     )
 
-    print(res.text)
+    # Check for token metadata
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    # Print token metadata
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    # Print response
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
