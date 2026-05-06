fe44aa7b65842116337f8be56349b65318be1fa2
ericstober <erictstober@gmail.com>
2026-05-05 22:57:20 -0500

    capture user prompt input and call the Gemini client using the user prompt

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import os
+import os, argparse
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    # Capture user input
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     # load environment variables from .env
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,10 +20,10 @@ def main():
     # Create new instance of a Gemini client
     client = genai.Client(api_key=api_key)
 
-    # Call client with model and prompt. Captured as response
+    # Call Gemini client with model and prompt. Captured as response
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=args.user_prompt,
     )
 
     # Check for token metadata
