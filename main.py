e2ef969a43b04aa4996b37944938c5a1836f3e35
ericstober <erictstober@gmail.com>
2026-06-05 21:55:47 -0500

    system prompts

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 
 def main():
@@ -30,6 +31,9 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt, temperature=0
+        ),
     )
 
     # Check for token metadata
