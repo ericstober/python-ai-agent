9430b528d9c9b584e71c0c0040d18202bb383059
ericstober <erictstober@gmail.com>
2026-06-13 11:24:59 -0500

    calling functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 def main():
@@ -49,8 +49,26 @@ def main():
 
     # Print response
     if response.function_calls:
+        function_results = []
+
         for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, verbose=args.verbose)
+
+            if not function_call_result.parts:
+                raise Exception("Function call result has no parts")
+
+            function_response = function_call_result.parts[0].function_response
+            if function_response is None:
+                raise Exception("Function call result has no function_response")
+
+            if function_response.response is None:
+                raise Exception("Function response has no response")
+
+            function_results.append(function_call_result.parts[0])
+
+            if args.verbose:
+                print(f"-> {function_response.response}")
+
     else:
         print(response.text)
 
